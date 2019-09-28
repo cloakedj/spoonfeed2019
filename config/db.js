f309b2f678617ea0524e0a34168ad5836583edf6
Dhananjay Kanwar <kanwardhananjaysingh0545@gmail.com>
2019-09-28 16:08:54 +0530

    Heroku changes

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri ="mongodb://localhost:27017/SFDB";
+const uri ="mongodb+srv://kanwardhananjay:1999101275D@cluster0-qinrs.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(() => console.log("Connection Succesful"))
     .catch(err => console.log(err));
