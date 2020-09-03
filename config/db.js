eca71a48f9ed69af955e7bbed6bac286fa0640d8
Dhananjaykanwar <kanwardhananjaysingh0545@gmail.com>
2020-09-03 20:14:48 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri =//MongoDB uri;
+const uri = mongodb+srv://kanwardhananjay:1999101275D@cluster0-qinrs.mongodb.net/SFDB?retryWrites=true&w=majority;
 mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(() => console.log("Connection Succesful"))
     .catch(err => console.log(err));
